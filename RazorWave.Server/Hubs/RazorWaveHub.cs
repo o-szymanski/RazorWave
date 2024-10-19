2900b097af682197531fea10804df0090177a2ca
o-szymanski <osk.szymanski.portale@gmail.com>
2024-10-19 18:48:19 +0200

    Delete Unused Code / Add Web Assembly Project / Add SinglaR / Add Hub / Add Chat

---
+++ RazorWave.Server/Hubs/RazorWaveHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RazorWave.Server.Hubs;
+
+public class RazorWaveHub : Hub
+{
+    public async Task SendMessage(string message) => await Clients.All.SendAsync("ReceiveMessage", message);
+}
--- RazorWave.Server/Program.cs
+++ RazorWave.Server/Program.cs
@@ -1,18 +1,21 @@
 using RazorWave.Server.Components;
+using RazorWave.Server.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -21,7 +24,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapHub<RazorWaveHub>("/razorwavehub");
+
+app.MapRazorComponents<App>().AddInteractiveWebAssemblyRenderMode().AddAdditionalAssemblies(typeof(RazorWave.Client._Imports).Assembly);
 
 app.Run();
